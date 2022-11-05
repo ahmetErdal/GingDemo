f3d867e6745b3295fea6827dbb84a9731492533e
iayka <78213760+iayka@users.noreply.github.com>
2022-11-05 19:52:32 +0300

    upp

--- Assets/Scripts/AIPlayerController.cs
+++ Assets/Scripts/AIPlayerController.cs
@@ -5,30 +5,5 @@ using UnityEngine;
 public class AIPlayerController : MonoBehaviour
 {
     public bool isFollowToMainPlayer;
-    //[HideInInspector] public GameObject target;
     
-    //private void Awake()
-    //{
-    //    isFollowToMainPlayer = false;
-    //}
-    //private void Update()
-    //{
-        
-    //}
-    //private void FixedUpdate()
-    //{
-    //    if (isFollowToMainPlayer)
-    //    {
-            
-    //        FollowToMainPlayer(target);
-           
-    //    }
-    //}
-
-    //public void FollowToMainPlayer(GameObject target)
-    //{
-       
-    //    transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime);
-    //    //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, .03f);
-    //}
 }
